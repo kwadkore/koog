a5dfbf8ef5d508809945f658a6e1e286b9e31201
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2026-01-08 12:36:08 +0100

    Change version to 0.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ import java.util.Base64
 version = run {
     // our version follows the semver specification
 
-    val baseVersion = "0.6.0-java-1"
+    val baseVersion = "0.7.0"
 
     val feat = run {
         val releaseBuild = !System.getenv("BRANCH_KOOG_IS_RELEASING_FROM").isNullOrBlank()
